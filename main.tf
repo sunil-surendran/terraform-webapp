7b26366bcc4c61f2e475cbb469dbe124c14ef021
Sunil Surendran <sunil.surendran@reancloud.com>
2018-06-26 15:33:06 +0530

    Added a security group. Also added a tfvars file to pass variables.

--- main.tf
+++ main.tf
@@ -2,21 +2,21 @@ provider "aws" {
   region = "us-west-2"
 }
 
-
 # VPC
 
 resource "aws_vpc" "sunil-tf-vpc" {
-  count = "${var.create_vpc ? 1 : 0}"
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
+  count                = "${var.create_vpc ? 1 : 0}"
+  cidr_block           = "${var.cidr}"
+  enable_dns_support   = true
   enable_dns_hostnames = true
-  instance_tenancy = "default"
+  instance_tenancy     = "default"
+
   tags {
-    Name = "sunil-tf1-vpc"
-    Owner = "sunil.surendran"
+    Name           = "sunil-tf1-vpc"
+    Owner          = "sunil.surendran"
     ExpirationDate = "2018-06-30"
-    Project = "Learning"
-    Environment = "Testing"
+    Project        = "Learning"
+    Environment    = "Testing"
   }
 }
 
@@ -29,40 +29,44 @@ resource "aws_internet_gateway" "sunil-tf-igw" {
 # SUBNET CONFIGURATION
 
 resource "aws_subnet" "public_a" {
-  availability_zone = "us-west-2a"
-  cidr_block = "10.0.1.0/24"
+  availability_zone       = "us-west-2a"
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  vpc_id = "${aws_vpc.sunil-tf-vpc.id}"
+  vpc_id                  = "${aws_vpc.sunil-tf-vpc.id}"
+
   tags {
     Name = "sunil-tf-pub-A"
   }
 }
 
 resource "aws_subnet" "public_b" {
-  availability_zone = "us-west-2b"
-  cidr_block = "10.0.2.0/24"
+  availability_zone       = "us-west-2b"
+  cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = true
-  vpc_id = "${aws_vpc.sunil-tf-vpc.id}"
+  vpc_id                  = "${aws_vpc.sunil-tf-vpc.id}"
+
   tags {
     Name = "sunil-tf-pub-B"
   }
 }
 
 resource "aws_subnet" "private_a" {
-  availability_zone = "us-west-2a"
-  cidr_block = "10.0.3.0/24"
+  availability_zone       = "us-west-2a"
+  cidr_block              = "10.0.3.0/24"
   map_public_ip_on_launch = false
-  vpc_id = "${aws_vpc.sunil-tf-vpc.id}"
+  vpc_id                  = "${aws_vpc.sunil-tf-vpc.id}"
+
   tags {
     Name = "sunil-tf-pri-A"
   }
 }
 
 resource "aws_subnet" "private_b" {
-  availability_zone = "us-west-2b"
-  cidr_block = "10.0.4.0/24"
+  availability_zone       = "us-west-2b"
+  cidr_block              = "10.0.4.0/24"
   map_public_ip_on_launch = false
-  vpc_id = "${aws_vpc.sunil-tf-vpc.id}"
+  vpc_id                  = "${aws_vpc.sunil-tf-vpc.id}"
+
   tags {
     Name = "sunil-tf-pri-B"
   }
@@ -75,11 +79,29 @@ resource "aws_route_table" "public_route_table" {
 }
 
 resource "aws_route" "public_route" {
-  route_table_id = "${aws_route_table.public_route_table.id}"
+  route_table_id         = "${aws_route_table.public_route_table.id}"
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = "${aws_internet_gateway.sunil-tf-igw.id}"
+  gateway_id             = "${aws_internet_gateway.sunil-tf-igw.id}"
 }
 
 resource "aws_route_table" "private_route_table" {
   vpc_id = "${aws_vpc.sunil-tf-vpc.id}"
+}
+
+resource "aws_security_group" "bastion_security" {
+    name = "bastion_security"
+    description = "Allow SSH access to bastion"
+    vpc_id = "${aws_vpc.sunil-tf-vpc.id}"
+    ingress {
+      from_port = 22
+      to_port = 22
+      protocol =  "tcp"
+      cidr_blocks = ["${var.secure_ip}"]
+    }
+    egress {
+      protocol = -1
+      cidr_blocks = ["0.0.0.0/0"]
+      from_port = 0
+      to_port = 0
+    }
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,14 @@
 variable "create_vpc" {
-  description = "Should VPC be created"
-  default = true
+  description = "Should VPC be created?"
+  default     = true
+}
+
+variable "cidr" {
+  description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overriden"
+  default     = "0.0.0.0/0"
+}   
+
+variable "secure_ip" {
+  description = "Secure IP to SSH into instances"
+  default = "61.12.88.78/32"
 }
