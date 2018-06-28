cb7e22e6601fc3833d10792c41559a131ae71ab7
Sunil Surendran <sunil.surendran@reancloud.com>
2018-06-28 22:31:26 +0530

    Completed the task

--- main.tf
+++ main.tf
@@ -23,58 +23,73 @@ resource "aws_vpc" "sunil-tf-vpc" {
 # INTERNET GATEWAY
 
 resource "aws_internet_gateway" "sunil-tf-igw" {
+  count  = "${var.create_vpc ? 1 : 0}"
   vpc_id = "${aws_vpc.sunil-tf-vpc.id}"
 }
 
 # SUBNET CONFIGURATION
 
-resource "aws_subnet" "public_a" {
-  availability_zone       = "us-west-2a"
-  cidr_block              = "10.0.1.0/24"
+resource "aws_subnet" "public" {
+  count                   = "${var.create_vpc==1 ? length(var.list_az) : 0 }"
+  availability_zone       = "${element(var.list_az, count.index)}"
+  cidr_block              = "${element(var.pub_cidr, count.index)}"
   map_public_ip_on_launch = true
   vpc_id                  = "${aws_vpc.sunil-tf-vpc.id}"
 
   tags {
-    Name = "sunil-tf-pub-A"
+    Name = "sunil-public-subnet-${count.index + 1}"
   }
 }
 
-resource "aws_subnet" "public_b" {
-  availability_zone       = "us-west-2b"
-  cidr_block              = "10.0.2.0/24"
-  map_public_ip_on_launch = true
-  vpc_id                  = "${aws_vpc.sunil-tf-vpc.id}"
-
-  tags {
-    Name = "sunil-tf-pub-B"
-  }
-}
-
-resource "aws_subnet" "private_a" {
-  availability_zone       = "us-west-2a"
-  cidr_block              = "10.0.3.0/24"
+resource "aws_subnet" "private" {
+  count                   = "${var.create_vpc==1 ? length(var.list_az) : 0 }"
+  availability_zone       = "${element(var.list_az, count.index)}"
+  cidr_block              = "${element(var.priv_cidr, count.index)}"
   map_public_ip_on_launch = false
   vpc_id                  = "${aws_vpc.sunil-tf-vpc.id}"
 
   tags {
-    Name = "sunil-tf-pri-A"
+    Name = "sunil-private-subnet-${count.index + 1}"
   }
 }
 
-resource "aws_subnet" "private_b" {
-  availability_zone       = "us-west-2b"
-  cidr_block              = "10.0.4.0/24"
-  map_public_ip_on_launch = false
-  vpc_id                  = "${aws_vpc.sunil-tf-vpc.id}"
-
-  tags {
-    Name = "sunil-tf-pri-B"
-  }
-}
-
+#resource "aws_subnet" "public_2" {
+#  availability_zone       = "us-west-2b"
+#  cidr_block              = "10.0.2.0/24"
+#  map_public_ip_on_launch = true
+#  vpc_id                  = "${aws_vpc.sunil-tf-vpc.id}"
+#
+#  tags {
+#    Name = "sunil-tf-pub-B"
+#  }
+#}
+#
+#resource "aws_subnet" "private_1" {
+#  availability_zone       = "us-west-2a"
+#  cidr_block              = "10.0.3.0/24"
+#  map_public_ip_on_launch = false
+#  vpc_id                  = "${aws_vpc.sunil-tf-vpc.id}"
+#
+#  tags {
+#    Name = "sunil-tf-pri-A"
+#  }
+#}
+#
+#resource "aws_subnet" "private_2" {
+#  availability_zone       = "us-west-2b"
+#  cidr_block              = "10.0.4.0/24"
+#  map_public_ip_on_launch = false
+#  vpc_id                  = "${aws_vpc.sunil-tf-vpc.id}"
+#
+#  tags {
+#    Name = "sunil-tf-pri-B"
+#  }
+#}
+#
 # ROUTE TABLE CONFIGURATION
 
 resource "aws_route_table" "public_route_table" {
+  count  = "${var.create_vpc ? 1 : 0}"
   vpc_id = "${aws_vpc.sunil-tf-vpc.id}"
 
   route {
@@ -88,6 +103,7 @@ resource "aws_route_table" "public_route_table" {
 }
 
 resource "aws_route_table" "private_route_table" {
+  count  = "${var.create_vpc ? 1 : 0}"
   vpc_id = "${aws_vpc.sunil-tf-vpc.id}"
 
   route {
@@ -100,33 +116,51 @@ resource "aws_route_table" "private_route_table" {
   }
 }
 
+# Private route for existing resource
+resource "aws_route" "private_r" {
+	count = "${var.create_vpc ? 0 : 1}"
+  route_table_id = "${var.private_rt}"
+  instance_id    = "${aws_instance.sunil_nat.id}"
+	destination_cidr_block = "0.0.0.0/0"
+}
+
 # SUBNET-ROUTE TABLE ASSOCIATION
 
-resource "aws_route_table_association" "public_a" {
-  subnet_id      = "${aws_subnet.public_a.id}"
+resource "aws_route_table_association" "public_route" {
+  count     = "${var.create_vpc==1 ? length(var.list_az) : 0 }"
+  subnet_id = "${element(concat(aws_subnet.public.*.id, list("")), count.index)}"
+
+  #subnet_id      = "${element(aws_subnet.public.*.id, count.index)}"
   route_table_id = "${aws_route_table.public_route_table.id}"
 }
 
-resource "aws_route_table_association" "public_b" {
-  subnet_id      = "${aws_subnet.public_b.id}"
-  route_table_id = "${aws_route_table.public_route_table.id}"
+#resource "aws_route_table_association" "public_2" {
+#  subnet_id      = "${aws_subnet.public_2.id}"
+#  route_table_id = "${aws_route_table.public_route_table.id}"
+#}
+
+resource "aws_route_table_association" "private_route" {
+  count = "${length(var.list_az)}"
+
+  #subnet_id      = "${list(join("", aws_subnet.private.*.id), count.index)}"
+  #subnet_id      = "${list(element(anatet.private.*.id, count.index))}"
+  subnet_id = "${var.create_vpc==1 ? element(concat(aws_subnet.private.*.id, list("")), count.index) : element(var.subnet_pri, count.index)}"
+
+  route_table_id = "${var.create_vpc==1 ? join("",  aws_route_table.private_route_table.*.id) : var.private_rt}"
 }
 
-resource "aws_route_table_association" "private_a" {
-  subnet_id      = "${aws_subnet.private_a.id}"
-  route_table_id = "${aws_route_table.private_route_table.id}"
-}
-
-resource "aws_route_table_association" "private_b" {
-  subnet_id      = "${aws_subnet.private_b.id}"
-  route_table_id = "${aws_route_table.private_route_table.id}"
-}
-
+#resource "aws_route_table_association" "private_2" {
+#  subnet_id      = "${aws_subnet.private_2.id}"
+#  route_table_id = "${aws_route_table.private_route_table.id}"
+#}
+#
 # SECURITY GROUP CONFIGURATIONS
 resource "aws_security_group" "bastion_security" {
   name        = "bastion_security"
   description = "Allow SSH access to bastion"
-  vpc_id      = "${aws_vpc.sunil-tf-vpc.id}"
+  vpc_id      = "${var.create_vpc==1 ? join("", aws_vpc.sunil-tf-vpc.*.id) : var.exist_vpc_id}"
+
+  #vpc_id      = "${var.create_vpc==1 ? aws_vpc.sunil-tf-vpc.id : var.exist_vpc_id}"
 
   ingress {
     from_port   = 22
@@ -134,14 +168,12 @@ resource "aws_security_group" "bastion_security" {
     protocol    = "tcp"
     cidr_blocks = ["${var.secure_ip}"]
   }
-
   egress {
     protocol    = -1
     cidr_blocks = ["0.0.0.0/0"]
     from_port   = 0
     to_port     = 0
   }
-
   tags {
     Name = "sunil_bastion_sg"
   }
@@ -150,29 +182,33 @@ resource "aws_security_group" "bastion_security" {
 resource "aws_security_group" "webserver_security" {
   name        = "webserver_security"
   description = "Allow SSH & HTTP access to VPC CIDR"
-  vpc_id      = "${aws_vpc.sunil-tf-vpc.id}"
+  vpc_id      = "${var.create_vpc==1 ? join("", aws_vpc.sunil-tf-vpc.*.id) : var.exist_vpc_id}"
+
+  #vpc_id      = "${var.create_vpc==1 ? aws_vpc.sunil-tf-vpc.id : var.exist_vpc_id}"
+
+
+  #vpc_id      = "${aws_vpc.sunil-tf-vpc.id}"
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
     cidr_blocks = ["${var.cidr}"]
   }
-
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+
     cidr_blocks = ["${var.cidr}"]
   }
-
   egress {
     protocol    = -1
     cidr_blocks = ["0.0.0.0/0"]
     from_port   = 0
     to_port     = 0
   }
-
   tags {
     Name = "sunil_instance_sg"
   }
@@ -181,15 +217,23 @@ resource "aws_security_group" "webserver_security" {
 resource "aws_security_group" "sunil_elb_sg" {
   name        = "sunil_elb_sg"
   description = "Security group for ELB"
-  vpc_id      = "${aws_vpc.sunil-tf-vpc.id}"
+  vpc_id      = "${var.create_vpc==1 ? join("", aws_vpc.sunil-tf-vpc.*.id) : var.exist_vpc_id}"
+
+  #vpc_id      = "${var.create_vpc==1 ? aws_vpc.sunil-tf-vpc.id : var.exist_vpc_id}"
+
+
+  #vpc_id      = "${aws_vpc.sunil-tf-vpc.id}"
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["${var.secure_ip}"]
-  }
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
 
+    cidr_blocks = ["${var.secure_ip}"]
+
+    #cidr_blocks = ["${split(",", var.create_vpc ==1 ? join(",", var.cidr) : join(",", var.exist_cidr))}"]
+    #cidr_blocks = ["${var.create_vpc==1 ? element(concat(var.cidr, list()), 0) : element(concat(var.exist_cidr, list()), 0)}"]
+  }
   egress {
     protocol    = -1
     cidr_blocks = ["0.0.0.0/0"]
@@ -201,36 +245,42 @@ resource "aws_security_group" "sunil_elb_sg" {
 resource "aws_security_group" "nat_security" {
   name        = "nat_security"
   description = "Access to internet for private instance"
-  vpc_id      = "${aws_vpc.sunil-tf-vpc.id}"
+  vpc_id      = "${var.create_vpc==1 ? join("", aws_vpc.sunil-tf-vpc.*.id) : var.exist_vpc_id}"
+
+  #vpc_id      = "${aws_vpc.sunil-tf-vpc.id}"
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["${var.cidr}"]
-  }
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
 
+    cidr_blocks = ["${var.cidr}"]
+
+    #cidr_blocks = ["${split(",", var.create_vpc==1 ? join(",", var.cidr) : join(",", var.exist_cidr))}"]
+    #cidr_blocks = ["${var.create_vpc==1 ? element(concat(var.cidr, list())) : element(concat(var.exist_cidr, list()))}"]
+  }
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["${var.cidr}"]
-  }
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
 
+    cidr_blocks = ["${var.cidr}"]
+
+    #cidr_blocks = ["${split(",", var.create_vpc==1 ? join(",", var.cidr) : join(",", var.exist_cidr))}"]
+    #cidr_blocks = ["${var.create_vpc==1 ? element(concat(var.cidr, list()), 0) : element(concat(var.exist_cidr, list()), 0)}"]
+  }
   ingress {
     from_port       = 0
     to_port         = 0
     protocol        = -1
     security_groups = ["${aws_security_group.webserver_security.id}"]
   }
-
   egress {
     protocol    = -1
     cidr_blocks = ["0.0.0.0/0"]
     from_port   = 0
     to_port     = 0
   }
-
   tags {
     Name = "sunil_nat_sg"
   }
@@ -240,11 +290,11 @@ resource "aws_security_group" "nat_security" {
 
 resource "aws_instance" "sunil_bastion" {
   ami                         = "${var.bastion_ami}"
-  availability_zone           = "us-west-2a"
+  availability_zone           = "${element(var.list_az, 0)}"
   instance_type               = "t2.micro"
-  key_name                    = "suniloregonec2"
+  key_name                    = "${var.keyname}"
   vpc_security_group_ids      = ["${aws_security_group.bastion_security.id}"]
-  subnet_id                   = "${aws_subnet.public_a.id}"
+  subnet_id                   = "${var.create_vpc==1 ? element(concat(aws_subnet.public.*.id, list("")), 0) : element(var.subnet_pub, 0)}"
   associate_public_ip_address = "true"
 
   tags {
@@ -257,13 +307,15 @@ resource "aws_instance" "sunil_bastion" {
 }
 
 resource "aws_instance" "sunil_nat" {
-  ami                         = "${var.nat_ami}"
-  availability_zone           = "us-west-2b"
-  instance_type               = "t2.micro"
-  key_name                    = "${var.keyname}"
-  source_dest_check           = "false"
-  vpc_security_group_ids      = ["${aws_security_group.nat_security.id}"]
-  subnet_id                   = "${aws_subnet.public_b.id}"
+  ami                    = "${var.nat_ami}"
+  availability_zone      = "${element(var.list_az, 1)}"
+  instance_type          = "t2.micro"
+  key_name               = "${var.keyname}"
+  source_dest_check      = "false"
+  vpc_security_group_ids = ["${aws_security_group.nat_security.id}"]
+  subnet_id              = "${var.create_vpc==1 ? element(concat(aws_subnet.public.*.id, list("")), 1) : element(var.subnet_pub, 1)}"
+
+  #subnet_id                   = "${aws_subnet.public_2.id}"
   associate_public_ip_address = "true"
 
   tags {
@@ -275,50 +327,55 @@ resource "aws_instance" "sunil_nat" {
   }
 }
 
-#resource "aws_instance" "sunil_webserver_a" {
-#  depends_on = [ "aws_instance.sunil_nat" ]
-#  ami = "${var.webserver_ami}"
-#  availability_zone = "us-west-2a"
-#  instance_type = "t2.micro"
-#  key_name = "${var.keyname}"
-#  vpc_security_group_ids = [ "${aws_security_group.webserver_security.id}" ]
-#  subnet_id = "${aws_subnet.private_a.id}"
-#  associate_public_ip_address = "false"
-#  tags {
-#    Name           = "sunil_webserver_a"
-#    Owner          = "sunil.surendran"
-#    ExpirationDate = "2018-06-30"
-#    Project        = "Learning"
-#    Environment    = "Testing"
-#  }
-#  user_data = "${file("userdata.sh")}"
-#}
+##
+###resource "aws_instance" "sunil_webserver_a" {
+###  depends_on = [ "aws_instance.sunil_nat" ]
+###  ami = "${var.webserver_ami}"
+###  availability_zone = "us-west-2a"
+###  instance_type = "t2.micro"
+###  key_name = "${var.keyname}"
+###  vpc_security_group_ids = [ "${aws_security_group.webserver_security.id}" ]
+###  subnet_id = "${aws_subnet.private_1.id}"
+###  associate_public_ip_address = "false"
+###  tags {
+###    Name           = "sunil_webserver_a"
+###    Owner          = "sunil.surendran"
+###    ExpirationDate = "2018-06-30"
+###    Project        = "Learning"
+###    Environment    = "Testing"
+###  }
+###  user_data = "${file("userdata.sh")}"
+###}
+##
+##resource "aws_instance" "sunil_webserver_b" {
+##  depends_on = [ "aws_instance.sunil_nat" ]
+##  ami = "${var.webserver_ami}"
+##  availability_zone = "us-west-2b"
+##  instance_type = "t2.micro"
+##  key_name = "${var.keyname}"
+##  vpc_security_group_ids = [ "${aws_security_group.webserver_security.id}" ]
+##  subnet_id = "${aws_subnet.private_2.id}"
+##  associate_public_ip_address = "false"
+##  tags {
+##    Name           = "sunil_webserver_b"
+##    Owner          = "sunil.surendran"
+##    ExpirationDate = "2018-06-30"
+##    Project        = "Learning"
+##    Environment    = "Testing"
+##  }
+##  user_data = "${file("userdata.sh")}"
+##}
 #
-#resource "aws_instance" "sunil_webserver_b" {
-#  depends_on = [ "aws_instance.sunil_nat" ]
-#  ami = "${var.webserver_ami}"
-#  availability_zone = "us-west-2b"
-#  instance_type = "t2.micro"
-#  key_name = "${var.keyname}"
-#  vpc_security_group_ids = [ "${aws_security_group.webserver_security.id}" ]
-#  subnet_id = "${aws_subnet.private_b.id}"
-#  associate_public_ip_address = "false"
-#  tags {
-#    Name           = "sunil_webserver_b"
-#    Owner          = "sunil.surendran"
-#    ExpirationDate = "2018-06-30"
-#    Project        = "Learning"
-#    Environment    = "Testing"
-#  }
-#  user_data = "${file("userdata.sh")}"
-#}
-
 # ELASTIC LOAD BALANCER
 
 resource "aws_elb" "sunil_elb" {
   name            = "sunil-elb"
   security_groups = ["${aws_security_group.sunil_elb_sg.id}"]
-  subnets         = ["${aws_subnet.public_a.id}", "${aws_subnet.public_b.id}"]
+
+  #	subnets         = ["${var.create_vpc==1 ? aws_subnet.public.*.id : var.subnet_pub}"]
+  subnets = ["${split(",", var.create_vpc ==1 ? join(",", aws_subnet.public.*.id) : join(",", var.subnet_pub))}"]
+
+  #	availability_zones = ["${var.list_az}"]
 
   #  instances = [ "${aws_instance.sunil_webserver_a.id}", "${aws_instance.sunil_webserver_b.id}" ]
   listener {
@@ -327,7 +384,6 @@ resource "aws_elb" "sunil_elb" {
     lb_port           = 80
     lb_protocol       = "http"
   }
-
   health_check {
     healthy_threshold   = 2
     unhealthy_threshold = 2
@@ -337,12 +393,15 @@ resource "aws_elb" "sunil_elb" {
   }
 }
 
+#
 # AUTO SCALING GROUP CONFIGURATION
 
 resource "aws_autoscaling_group" "webserver_group" {
-  max_size             = 4
-  min_size             = 4
-  vpc_zone_identifier  = ["${aws_subnet.private_a.id}", "${aws_subnet.private_b.id}"]
+  max_size = 4
+  min_size = 2
+
+  #  vpc_zone_identifier  = ["${aws_subnet.private.*.id}"]
+  vpc_zone_identifier  = ["${split(",", var.create_vpc ==1 ? join(",", aws_subnet.private.*.id) : join(",", var.subnet_pri))}"]
   load_balancers       = ["${aws_elb.sunil_elb.id}"]
   launch_configuration = "${aws_launch_configuration.sunil_launch_asg.name}"
 
--- variables.tf
+++ variables.tf
@@ -1,12 +1,50 @@
 variable "create_vpc" {
-  description = "Should VPC be created?"
-  default     = true
+  description = "Should VPC be created? (ENTER 1 for yes; 0 for no)"
 }
 
+variable "region" {
+ description = "Region to launch the template"
+}
+
+variable "list_az" {
+	description = "The Availability Zones for the template"
+	type = "list"
+}
+
+
+
+############# NEW ARCHITECTURE CIDR RANGES
 variable "cidr" {
   description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overriden"
   default     = "0.0.0.0/0"
 }   
+
+variable "pub_cidr" {
+  description = "CIDR for Public subnet"
+	type = "list"
+}   
+
+variable "priv_cidr" {
+  description = "CIDR for Private subnet"
+	type = "list"
+}   
+
+##################EXISTING ARCHITECTURE CIDR RANGES ####################
+#variable "exist_cidr" {
+#  description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overriden"
+#  default     = "0.0.0.0/0"
+#}   
+#
+#variable "exist_pub_cidr" {
+#  description = "CIDR for Public subnet"
+#	type = "list"
+#}   
+#
+#variable "exist_priv_cidr" {
+#  description = "CIDR for Private subnet"
+#	type = "list"
+#}   
+
 
 variable "secure_ip" {
   description = "Secure IP to SSH into instances"
@@ -31,4 +69,20 @@ variable "nat_ami" {
 variable "keyname" {
   description = "KeyName for account"
   default = "suniloregonec2"
+}
+
+variable "exist_vpc_id" {
+	default = "vpc-50c0d829"
+}
+
+variable "subnet_pub" {
+	type = "list"
+}
+
+variable "subnet_pri" {
+	type = "list"
+}
+
+variable "private_rt" {
+	default = "rtb-192ddc62"
 }
